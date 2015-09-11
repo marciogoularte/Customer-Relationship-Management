88911753dc5e8e61a245bc66c265ed31c67ea808
Nikolay-D <dermendzhiev.n@gmail.com>
2015-09-11 12:10:15 +0300

    Modify client model

--- Data/TVChannelsCRM.Data.Models/Client.cs
+++ Data/TVChannelsCRM.Data.Models/Client.cs
@@ -90,37 +90,6 @@
 
         public string MarketingEmail { get; set; }
 
-        public bool IsActive { get; set; }
-
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime? ActiveFrom { get; set; }
-
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime? ActiveTo { get; set; }
-
-        //public int MyProperty { get; set; }
-
-        public string Mg { get; set; }
-
-        public string IrdCard { get; set; }
-
-        // TODO: work on it, probably invoicing with enumeration
-        public string Invoicing { get; set; }
-
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime? DateOfSigning { get; set; }
-
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
-        public DateTime? DateOfExpiring { get; set; }
-
-        public string Currency { get; set; }
-
-        public string InvoicesIssued { get; set; }
-
-        public string PaymentsReceived { get; set; }
-
-        public string Contract { get; set; }
-
         public ICollection<Channel> Channels
         {
             get { return this.channels; }
