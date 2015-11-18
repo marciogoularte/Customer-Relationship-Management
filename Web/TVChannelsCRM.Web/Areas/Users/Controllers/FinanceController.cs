dbc9d62ebc441a3756eb7f5219ece20198cd10a2
Nikolay-D <dermendzhiev.n@gmail.com>
2015-11-18 16:48:34 +0200

    Add "Products" and "Finance" controllers

---
+++ Web/TVChannelsCRM.Web/Areas/Users/Controllers/FinanceController.cs
@@ -0,0 +1,35 @@
+﻿namespace TVChannelsCRM.Web.Areas.Users.Controllers
+{
+    using System.Web.Mvc;
+
+    using Data;
+    using Web.Controllers;
+
+    public class FinanceController : BaseController
+    {
+        public FinanceController(ITVChannelsCRMData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Invoicing()
+        {
+            return View();
+        }
+
+        public ActionResult Payments()
+        {
+            return View();
+        }
+
+        public ActionResult Reports()
+        {
+            return View();
+        }
+
+        public ActionResult Frz()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/TVChannelsCRM.Web/Areas/Users/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿namespace TVChannelsCRM.Web.Areas.Users.Controllers
+{
+    using System.Web.Mvc;
+
+    using Data;
+    using Web.Controllers;
+
+    public class ProductsController : BaseController
+    {
+        public ProductsController(ITVChannelsCRMData data)
+            : base(data)
+        {
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
